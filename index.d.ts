d08b4e35d732555a273c0072c9cbc70c5581d681
tohsaka888 <156132264@qq.com>
2022-07-15 15:20:23 +0800

    feat: 图谱类型

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare namespace Graph {
+  type Node = {
+    id: string;
+    name: string;
+    type: string;
+    mode: number; // 0 主节点 1 入边 2 出边
+  }
+
+  type Edge = {
+    fromId: string;
+    toId: string;
+    discription: string;
+  }
+}
