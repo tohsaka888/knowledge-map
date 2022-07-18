521d93fc1b7c3b45107ab71f1a2112a8aba59fc1
tohsaka888 <156132264@qq.com>
2022-07-18 11:02:25 +0800

    fix: 修复分页可能有值不存在

--- components/KnowledgeMap/drawEdgeArea.ts
+++ components/KnowledgeMap/drawEdgeArea.ts
@@ -1,6 +1,14 @@
 import * as d3 from 'd3'
 
-export const drawEdgeArea = (container: SVGGElement, edges: Graph.Edge[]) => {
+// const calcEdges = (edges: Graph.Edge[], mode: number | undefined) => {
+//   if (mode === 1 || !mode) {
+//     return edges
+//   } else {
+//     return edges.slice(0, 5)
+//   }
+// }
+
+export const drawEdgeArea = (container: SVGGElement, edges: Graph.Edge[], mode: number | undefined) => {
   d3.select(container)
     .insert('g', ':first-child')
     .selectAll('g')
@@ -10,7 +18,7 @@ export const drawEdgeArea = (container: SVGGElement, edges: Graph.Edge[]) => {
     .attr('d', function (edge) {
       const fromNode = d3.select(`#${edge.fromId}`)
       const toNode = d3.select(`#${edge.toId}`)
-      return `M ${fromNode.attr('cx')} ${fromNode.attr('cy')} L ${toNode.attr('cx')} ${toNode.attr('cy')}`
+      return fromNode.nodes().length !== 0 && toNode.nodes().length !== 0 && `M ${fromNode.attr('cx')} ${fromNode.attr('cy')} L ${toNode.attr('cx')} ${toNode.attr('cy')}`
     })
     .attr('stroke-width', 1)
     .attr('stroke', '#cecece')
