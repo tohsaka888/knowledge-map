c89febc47c4ffa2ea017df32e28464c231ba8c80
tohsaka888 <156132264@qq.com>
2022-07-20 17:19:08 +0800

    ✨ feat(edge): 增加关系描述

--- components/KnowledgeMap/drawEdgeArea.ts
+++ components/KnowledgeMap/drawEdgeArea.ts
@@ -1,4 +1,5 @@
 import * as d3 from 'd3'
+import style from './index.module.css'
 import { Graph } from '../..'
 
 /**
@@ -12,14 +13,19 @@ export const drawEdgeArea = (edges: Graph.Edge[], centerPointId?: string) => {
   const mainPoint = d3.select(centerPointId ? `#${centerPointId}` : '#main')
   const edgeArea = d3.select('#edge-area')
     .insert('g', ':first-child')
-  edgeArea
+  const edge = edgeArea
     .selectAll('g')
     .data(edges)
     .join('g')
     .filter((item) => d3.select(`#${item.fromId}${item.toId}`).nodes().length === 0)
     .classed('edge', true)
+  edge
     .append('path')
-    .attr('d', `m ${mainPoint.attr('x')} 400 l ${mainPoint.attr('x')} 400 l ${mainPoint.attr('x')} 400`)
+    .attr('d',
+      `m ${mainPoint.attr('x')} ${mainPoint.attr('y')} 
+       l ${mainPoint.attr('x')} ${mainPoint.attr('y')} 
+       l ${mainPoint.attr('x')} ${mainPoint.attr('y')}`
+    )
     .attr('stroke-width', 1)
     .attr('stroke', '#cecece')
     .attr('id', item => item.fromId + item.toId)
@@ -42,4 +48,17 @@ export const drawEdgeArea = (edges: Graph.Edge[], centerPointId?: string) => {
           `
         : `M 0 0`
     })
+  edge
+    .append('text')
+    .append('textPath')
+    .style('opacity', 0)
+    .attr('text-anchor', 'center')
+    .attr('href', item => `#${item.fromId + item.toId}`)
+    .classed(style['discription'], true)
+    .attr('startOffset', '50%')
+    .attr('dy', 20)
+    .text(item => item.discription)
+    .transition()
+    .duration(1000)
+    .style('opacity', 1)
 }
