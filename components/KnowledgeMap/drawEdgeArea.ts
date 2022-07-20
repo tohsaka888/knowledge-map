42684eb44ebc562b75661fb087ad95c215515b76
tohsaka888 <156132264@qq.com>
2022-07-20 14:12:39 +0800

    🎈 perf(edge): 绘制边添加动画效果

--- components/KnowledgeMap/drawEdgeArea.ts
+++ components/KnowledgeMap/drawEdgeArea.ts
@@ -8,7 +8,9 @@ import { Graph } from '../..'
  * @param {any} mode:number|undefined
  * @returns {any}
  */
-export const drawEdgeArea = (edges: Graph.Edge[]) => {
+export const drawEdgeArea = (edges: Graph.Edge[], centerPointId?: string) => {
+  const mainPoint = d3.select(centerPointId ? `#${centerPointId}` : '#main')
+  console.log(mainPoint.attr('x'))
   const edgeArea = d3.select('#edge-area')
     .insert('g', ':first-child')
   edgeArea
@@ -17,25 +19,27 @@ export const drawEdgeArea = (edges: Graph.Edge[]) => {
     .join('g')
     .classed('edge', true)
     .append('path')
+    .attr('d', `m ${mainPoint.attr('x')} 400 l ${mainPoint.attr('x')} 400 l ${mainPoint.attr('x')} 400`)
+    .attr('stroke-width', 1)
+    .attr('stroke', '#cecece')
+    .attr('id', item => item.fromId + item.toId)
+    .style('marker-mid', 'url(#arrow)')
+    .transition()
+    .duration(1000)
     .attr('d', function (edge) {
       const fromNode = d3.select(`#${edge.fromId}`)
       const toNode = d3.select(`#${edge.toId}`)
       let middlePointX = 0, middlePointY = 0
       if (fromNode.nodes().length !== 0 && toNode.nodes().length !== 0) {
-        middlePointX = (+fromNode.attr('cx') + +toNode.attr('cx')) / 2
-        middlePointY = (+fromNode.attr('cy') + +toNode.attr('cy')) / 2
+        middlePointX = (+fromNode.attr('x') + +toNode.attr('x')) / 2
+        middlePointY = (+fromNode.attr('y') + +toNode.attr('y')) / 2
       }
       return fromNode.nodes().length !== 0 && toNode.nodes().length !== 0
         ? `
-          M ${fromNode.attr('cx')} ${fromNode.attr('cy')} 
+          M ${fromNode.attr('x')} ${fromNode.attr('y')} 
           L ${middlePointX} ${middlePointY} 
-          M ${middlePointX} ${middlePointY} 
-          L ${toNode.attr('cx')} ${toNode.attr('cy')}
+          L ${toNode.attr('x')} ${toNode.attr('y')}
           `
         : `M 0 0`
     })
-    .attr('stroke-width', 1)
-    .attr('stroke', '#cecece')
-    .attr('id', item => item.fromId + item.toId)
-    .style('marker-mid', 'url(#arrow)')
 }
