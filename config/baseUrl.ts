ef34e5790eb9c3603577cd83997dead7ec789a08
tohsaka888 <156132264@qq.com>
2022-07-20 14:15:40 +0800

    ✨ feat(config): 配置主路由

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,1 +1,4 @@
-export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://knowledge-map.vercel.app' : 'http://localhost:3000'
+export const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://knowledge-map.vercel.app'
+    : 'http://localhost:3000'
