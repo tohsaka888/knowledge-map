ac4db2911101aeab0a553aa7430667b6584fd01b
tohsaka888 <156132264@qq.com>
2022-08-08 09:34:38 +0800

    ✨ feat: 清除画布时同时清除记住的节点坐标

--- components/KnowledgeMap/EdgeArea.tsx
+++ components/KnowledgeMap/EdgeArea.tsx
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-08-01 11:31:01
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-03 17:08:28
+ * @LastEditTime: 2022-08-08 09:25:21
  * @Description: 请填写简介
  */
 import React, { ReactNode, useEffect, useRef } from 'react'
@@ -10,17 +10,17 @@ import { drawEdgeArea } from './drawEdgeArea';
 import * as d3 from 'd3'
 import { Graph } from '../..';
 
-function EdgeArea({ children, edges, config }: { children: ReactNode; edges: Graph.Edge[], config: Graph.ConfigProps }) {
+function EdgeArea({ children,nodes, edges, config }: { children: ReactNode; nodes: Graph.Node[]; edges: Graph.Edge[], config: Graph.ConfigProps }) {
   const edgeAreaContainer = useRef<SVGGElement>(null!)
 
   useEffect(() => {
     const container = edgeAreaContainer.current
-    drawEdgeArea(edges, config)
+    drawEdgeArea(nodes, edges, config)
     return () => {
       d3.select(container).selectAll(':first-child').remove()
       d3.select('#selector-result').remove()
     }
-  }, [edges, config])
+  }, [edges, config, nodes])
 
   return (
     <g ref={edgeAreaContainer} id="edge-area">
