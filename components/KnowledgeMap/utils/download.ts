69ce3fa2adb25d9e108ebd2af4750397c8d9e9a7
tohsaka888 <156132264@qq.com>
2022-08-15 14:41:42 +0800

    ✨ feat: 支持下载整张图

--- components/KnowledgeMap/utils/download.ts
+++ components/KnowledgeMap/utils/download.ts
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-08-04 10:36:41
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-04 13:48:01
+ * @LastEditTime: 2022-08-15 14:40:28
  * @Description: 请填写简介
  */
 import * as d3 from 'd3'
@@ -11,10 +11,18 @@ import { base64ToBlob } from './base64ToBlob';
 
 export const downloadSvg = () => {
   const svg = d3.select('#svg').node() as HTMLElement
-  const width = document.getElementById('drag')?.getBoundingClientRect().width as number
-  const height = document.getElementById('drag')?.getBoundingClientRect().height as number
+  const drag = document.getElementById('drag')!
+  const width = drag.getBoundingClientRect().width as number + 50
+  const height = drag.getBoundingClientRect().height as number + 50
 
-  const s = new XMLSerializer().serializeToString(svg);
+  const newSvg = svg.cloneNode(true)
+  d3.select(newSvg as SVGSVGElement)
+    .select('#drag')
+    .attr('transform', `translate(${-drag.getBoundingClientRect().left + 225}, ${-drag.getBoundingClientRect().top + 85})`)
+
+  console.log(newSvg)
+
+  const s = new XMLSerializer().serializeToString(newSvg);
   const src = `data:image/svg+xml;base64,${window.btoa(unescape(encodeURIComponent(s)))}`;
 
   const img = new Image(); // 创建图片容器承载过渡
@@ -24,8 +32,8 @@ export const downloadSvg = () => {
   img.onload = () => {
     // ↓ 第二部分
     const canvas = document.createElement('canvas');
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight;
+    canvas.width = width
+    canvas.height = height
     const context = canvas.getContext('2d');
     context?.drawImage(img, 0, 0);
     const ImgBase64 = canvas.toDataURL('image/png');
