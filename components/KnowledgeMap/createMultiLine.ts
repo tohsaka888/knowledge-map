e640dc118febed83425e7f1eeebf15cf7e53e258
tohsaka888 <156132264@qq.com>
2022-08-16 11:53:36 +0800

    ✨ feat: 支持两个节点之间创建多条连线

---
+++ components/KnowledgeMap/createMultiLine.ts
@@ -0,0 +1,44 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-08-16 11:47:14
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-08-16 11:49:16
+ * @Description: 请填写简介
+ */
+
+import { Graph } from "../..";
+import { createDescription } from "./createDescription";
+
+type Props = {
+  isTop: boolean;
+  topCount: number;
+  downCount: number;
+  fromNode: Graph.Node;
+  toNode: Graph.Node;
+  edgeArea: d3.Selection<any, any, any, any>;
+  edge: Graph.Edge;
+  config: Graph.ConfigProps
+}
+
+export const createMultiLine = ({ isTop, topCount, downCount, fromNode, toNode, edgeArea, edge, config }: Props) => {
+  if (isTop) {
+    topCount++
+  } else {
+    downCount++
+  }
+  // 中间点
+  const angle = Math.PI / 2 - Math.atan2(fromNode.y! - toNode.y!, toNode.x! - fromNode.x!)
+  const dx = -Math.cos(angle) * (isTop ? topCount * 38 : downCount * 38)
+  const dy = -Math.sin(angle) * (isTop ? topCount * 38 : downCount * 38)
+  const middlePoint = { x: (fromNode.x! + toNode.x! + dx) / 2 + dx, y: (fromNode.y! + toNode.y!) / 2 + dy }
+  edgeArea.append('path')
+    .attr('d', `
+      M ${fromNode.x} ${fromNode.y},
+      Q ${middlePoint.x} ${middlePoint.y}, ${toNode.x} ${toNode.y}
+    `)
+    .attr('fill', 'transparent')
+    .attr('stroke', '#cecece')
+    .attr('stroke-width', 1)
+    .attr('id', edge.fromId + edge.toId + (isTop ? ('top' + topCount) : ('down' + downCount)))
+  createDescription({ edge, edgeId: edge.fromId + edge.toId + (isTop ? ('top' + topCount) : ('down' + downCount)), edgeArea, config })
+}
--- components/KnowledgeMap/drawEdgeArea.ts
+++ components/KnowledgeMap/drawEdgeArea.ts
@@ -2,12 +2,13 @@
  * @Author: tohsaka888
  * @Date: 2022-08-01 11:31:01
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-16 11:37:58
+ * @LastEditTime: 2022-08-16 11:52:27
  * @Description: 请填写简介
  */
 import * as d3 from 'd3'
 import { Graph } from '../..'
 import { createDescription } from './createDescription'
+import { createMultiLine } from './createMultiLine'
 
 /**
  * 画直线
@@ -23,26 +24,36 @@ const drawStraightLine = (
   config: Graph.ConfigProps,
   edgeArea: d3.Selection<any, unknown, any, any>
 ) => {
+  let topCount = 0
+  let downCount = 0
   edges.forEach((edge) => {
     const fromNode = nodes.find(node => node.id === edge.fromId)
     const toNode = nodes.find(node => node.id === edge.toId)
     if (fromNode && toNode) {
-      const current = edgeArea
-        .append('path')
-        .attr('d',
-          `m ${mainPoint.x} ${mainPoint.y} 
+      // 判断之前两个节点之间是否已经有连线
+      const isExist = document.getElementById(fromNode.id + toNode.id) || document.getElementById(toNode.id + fromNode.id)
+      // 判断当前连线在上方or下方
+      const isTop = (fromNode.x as number) < (toNode.x as number)
+      if (isExist) {
+        createMultiLine({ isTop, fromNode, toNode, topCount, downCount, edgeArea, config, edge })
+      } else {
+        const current = edgeArea
+          .append('path')
+          .attr('d',
+            `m ${mainPoint.x} ${mainPoint.y} 
        l ${mainPoint.x} ${mainPoint.y} 
        l ${mainPoint.x} ${mainPoint.y}`
-        )
-        .attr('stroke-width', config.lineWidth)
-        .attr('stroke', '#cecece')
-        .attr('id', edge.fromId + edge.toId)
-        .style('opacity', 0);
-      current.transition()
-        .duration(1000)
-        .style('opacity', 1)
-        .attr('d', `M ${fromNode.x} ${fromNode.y},L ${toNode.x} ${toNode.y}`);
-      createDescription({ edgeArea, config, edge, edgeId: edge.fromId + edge.toId })
+          )
+          .attr('stroke-width', config.lineWidth)
+          .attr('stroke', '#cecece')
+          .attr('id', edge.fromId + edge.toId)
+          .style('opacity', 0);
+        current.transition()
+          .duration(1000)
+          .style('opacity', 1)
+          .attr('d', `M ${fromNode.x} ${fromNode.y},L ${toNode.x} ${toNode.y}`);
+        createDescription({ edgeArea, config, edge, edgeId: edge.fromId + edge.toId })
+      }
     }
   })
 }
@@ -61,34 +72,44 @@ const drawBesselLine = (
   config: Graph.ConfigProps,
   edgeArea: d3.Selection<any, unknown, any, any>
 ) => {
+  let topCount = 0;
+  let downCount = 0;
   edges.forEach((edge, index) => {
     const fromNode = nodes.find(node => node.id === edge.fromId)
     const toNode = nodes.find(node => node.id === edge.toId)
     let perX = 0
     if (fromNode && toNode && fromNode.x && toNode.x) {
-      perX = (toNode.x - fromNode.x) / config.besselRate
-      edgeArea.append('path')
-        .attr('d',
-          `m ${mainPoint.x} ${mainPoint.y} 
+      // 判断之前两个节点之间是否已经有连线
+      const isExist = document.getElementById(fromNode.id + toNode.id) || document.getElementById(toNode.id + fromNode.id)
+      // 判断当前连线在上方or下方
+      const isTop = (fromNode.x as number) < (toNode.x as number)
+      if (isExist) {
+        createMultiLine({ fromNode, toNode, isTop, edge, edgeArea, topCount, downCount, config })
+      } else {
+        perX = (toNode.x - fromNode.x) / config.besselRate
+        edgeArea.append('path')
+          .attr('d',
+            `m ${mainPoint.x} ${mainPoint.y} 
        l ${mainPoint.x} ${mainPoint.y} 
        l ${mainPoint.x} ${mainPoint.y}
       `
-        )
-        .attr('stroke-width', config.lineWidth)
-        .attr('stroke', '#cecece')
-        .attr('fill', 'transparent')
-        .attr('id', edge.fromId + edge.toId)
-        .style('opacity', 0)
-        .transition()
-        .duration(1000)
-        .style('opacity', 1)
-        .attr('d', `
+          )
+          .attr('stroke-width', config.lineWidth)
+          .attr('stroke', '#cecece')
+          .attr('fill', 'transparent')
+          .attr('id', edge.fromId + edge.toId)
+          .style('opacity', 0)
+          .transition()
+          .duration(1000)
+          .style('opacity', 1)
+          .attr('d', `
       M ${fromNode.x} ${fromNode.y},
       C ${+fromNode.x + perX} ${fromNode.y},
       ${+toNode.x - perX} ${toNode.y}
       ${toNode.x} ${toNode.y}
       `)
-      createDescription({ edgeArea, config, edge, edgeId: edge.fromId + edge.toId })
+        createDescription({ edgeArea, config, edge, edgeId: edge.fromId + edge.toId })
+      }
     }
   })
 }
