cd1f012db09b63241146db39fb75ebd0f374d9ff
tohsaka888 <156132264@qq.com>
2022-08-16 17:26:33 +0800

    ✨ feat: 借助闭包特性

--- components/KnowledgeMap/nodeDrag.ts
+++ components/KnowledgeMap/nodeDrag.ts
@@ -2,10 +2,26 @@
  * @Author: tohsaka888
  * @Date: 2022-08-16 17:22:12
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-16 17:22:13
+ * @LastEditTime: 2022-08-16 17:25:48
  * @Description: 请填写简介
  */
 
+// 借助闭包特性
 export const nodeDrag = () => {
-  
+  // 开始拖动
+  const dragStart = () => {
+
+  }
+
+  // 拖动中
+  const dragging = () => {
+    requestAnimationFrame(() => {
+
+    })
+  }
+
+  // 结束拖动
+  const dragEnd = () => {
+
+  }
 }
