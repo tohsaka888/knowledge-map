706e22febb6ca5a76c36e9e128c2f5409db909dc
tohsaka888 <156132264@qq.com>
2022-08-16 16:48:36 +0800

    🎈 perf: 重构代码,优化性能

--- components/KnowledgeMap/drawSideNodes.ts
+++ components/KnowledgeMap/drawSideNodes.ts
@@ -19,6 +19,7 @@ import { calcArcX, calcArcY } from "./utils/calcArc";
 import { calcBasicDistence } from "./utils/calcBasicDistance";
 import { transferLabelName } from "./utils/transferLabelName";
 import { verticePrefix } from "./prefix";
+import { createSideNode } from "./createNode";
 
 type Props = {
   typeNodes: Graph.Vertice[][];
@@ -41,7 +42,6 @@ export const drawSideNodes = (
 ) => {
   const { nodeRadius, arcAreaDistence, arcAreaLength, mode, basicDistence, setVisible } = config
   typeNodes.forEach((originNodes, index) => {
-    console.log(originNodes, maxAngle)
     const nodes = calcMode(originNodes, 1, mode)
     const pagination = { page: 1, pageSize: 5 }
     if (mode === 2 && originNodes.length > 5) {
@@ -101,107 +101,53 @@ export const drawSideNodes = (
         .attr('id', `${verticePrefix + nodes[0].labelName}-text`)
         .attr('text-anchor', 'right')
     }
-    const sideContainer = d3.select(container)
+
+    const typeContainer = d3.select(container)
       .append('g')
       .attr('id', verticePrefix + nodes[0].labelName)
-      .selectAll('g')
-      .data(nodes)
-      .join('g')
-
-    window.setTimeout(() => {
-      sideContainer.on('mouseover', function (event, d) {
-        event.stopPropagation();
-        const node = d3.select(`#${d.id}`)
-        const x = +node.attr('cx')
-        const y = +node.attr('cy')
-        d3.select('#border')
-          .attr('transform', `translate(${x}, ${y})`)
-          .select('circle')
-          .attr('stroke-width', 8)
-          .attr('r', nodeRadius + 4)
-        d3.select('#popover-container')
-          .attr('width', nodeRadius * 2 + 10)
-          .attr('height', nodeRadius * 2 + 10)
-          .attr('x', +x)
-          .attr('y', +y - 10)
-        setVisible && setVisible(true)
-      })
-      // .call(
-      //   d3.drag<any, any, Graph.Node>()
-      //     .on('start', function (event: any, node: Graph.Node) {
-      //       dragStart(this, event, node, config)
-      //     })
-      //     .on('end', function (event: any, node: Graph.Node) {
-      //       dragEnd(this, event, node, config)
-      //     })
-      //     .on('drag', function (event: any, node: Graph.Node) {
-      //       dragging(this, event, node, edges, config)
-      //     })
-      // )
-    }, 1000)
-
-    sideContainer
-      .append('circle')
-      .attr('r', nodeRadius)
-      .attr('fill', '#1890ff')
-      .attr('id', (node) => verticePrefix + node.id)
-      .attr('cx', centerPoint.x!)
-      .attr('cy', centerPoint.y!)
-      .transition()
-      .duration(1000)
-      .attr('cx', (node, idx) => {
-        const x = centerPoint.x!
-        const angle: number = (idx + 1) * maxAngle / (nodes.length + 1) + index * maxAngle
-        node.x = isInside
-          ? x - calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.cos(Math.abs(angle - 90) / 180 * Math.PI)
-          : x + calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.cos(Math.abs(angle - 90) / 180 * Math.PI)
-        return node.x
-      })
-      .attr('cy', (node, idx) => {
-        const y = centerPoint.y!
-        const angle: number = (idx + 1) * maxAngle / (nodes.length + 1) + index * maxAngle
-        node.y = isInside
-          ? y + calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.sin((angle - 90) / 180 * Math.PI)
-          : y + calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.sin((angle - 90) / 180 * Math.PI)
-        return node.y
-      })
-
-    sideContainer
-      .append('text')
-      .attr('id', node => verticePrefix + node.id + 'text')
-      .style('cursor', 'pointer')
-      .attr('text-anchor', 'middle')
-      .attr('dominant-baseline', 'middle')
-      .attr('fill', '#fff')
-      .style('font-size', 14)
-      .attr('x', centerPoint.x!)
-      .attr('y', centerPoint.y!)
-      .transition()
-      .duration(1000)
-      .attr('x', (node, idx) => {
-        return node.x as number
-      })
-      .attr('y', (node, idx) => {
-        return node.y as number
-      })
-      .text(node => transferLabelName(node.labelName))
-    sideContainer
-      .append('text')
-      .attr('x', centerPoint.x!)
-      .attr('y', centerPoint.y! + nodeRadius + 10)
-      .attr('text-anchor', 'middle')
-      .style('font-size', 10)
-      .text(item => item.name)
-      .attr('id', item => `${verticePrefix + item.id}name`)
-      .style('opacity', 0)
-      .transition()
-      .duration(1000)
-      .attr('x', (node, idx) => {
-        return node.x as number
-      })
-      .attr('y', (node, idx) => {
-        return node.y as number + nodeRadius + 10
-      })
-      .style('opacity', 1)
+    nodes.forEach((node, idx) => {
+      // 
+      const x = centerPoint.x!
+      const y = centerPoint.y!
+      const angle: number = (idx + 1) * maxAngle / (nodes.length + 1) + index * maxAngle
+      node.x = isInside
+        ? x - calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.cos(Math.abs(angle - 90) / 180 * Math.PI)
+        : x + calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.cos(Math.abs(angle - 90) / 180 * Math.PI)
+      node.y = isInside
+        ? y + calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.sin((angle - 90) / 180 * Math.PI)
+        : y + calcBasicDistence(nodes.length, maxAngle, basicDistence) * Math.sin((angle - 90) / 180 * Math.PI)
+      const sideContainer = typeContainer.append('g')
+      createSideNode({ container: sideContainer, vertice: node, mainVertice: centerPoint, config })
+      window.setTimeout(() => {
+        sideContainer.on('mouseover', function (event) {
+          event.stopPropagation();
+          const x = node.x as number
+          const y = node.y as number
+          d3.select('#border')
+            .attr('transform', `translate(${x}, ${y})`)
+            .select('circle')
+            .attr('stroke-width', 8)
+            .attr('r', nodeRadius + 4)
+          d3.select('#popover-container')
+            .attr('width', nodeRadius * 2 + 10)
+            .attr('height', nodeRadius * 2 + 10)
+            .attr('x', +x)
+            .attr('y', +y - 10)
+          setVisible && setVisible(true)
+        })
+        // .call(
+        //   d3.drag<any, any, Graph.Node>()
+        //     .on('start', function (event: any, node: Graph.Node) {
+        //       dragStart(this, event, node, config)
+        //     })
+        //     .on('end', function (event: any, node: Graph.Node) {
+        //       dragEnd(this, event, node, config)
+        //     })
+        //     .on('drag', function (event: any, node: Graph.Node) {
+        //       dragging(this, event, node, edges, config)
+        //     })
+        // )
+      }, 1000)
+    })
   })
 }
