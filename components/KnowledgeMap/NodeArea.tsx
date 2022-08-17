5113dd930199fab4279f26f676cb437bf158ea38
tohsaka888 <156132264@qq.com>
2022-08-17 16:10:20 +0800

    🦄 refactor: 重构点代码

--- components/KnowledgeMap/NodeArea.tsx
+++ components/KnowledgeMap/NodeArea.tsx
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-08-08 08:29:23
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-17 08:20:21
+ * @LastEditTime: 2022-08-17 10:28:40
  * @Description: 请填写简介
  */
 import React, { useContext, useEffect, useRef } from 'react'
@@ -27,7 +27,19 @@ function NodeArea({ mainVertice, insideVertices, outsideVertices, edges, config 
   // 状态改变时清除画布
   useEffect(() => {
     const container = nodesContainerRef.current
-    drawNodeArea({ container, mainVertice, insideVertices, outsideVertices, config: { ...config, setVisible }, x: 700, y: 400, init: true })
+    drawNodeArea(
+      {
+        container,
+        mainVertice,
+        insideVertices,
+        outsideVertices,
+        config: { ...config, setVisible },
+        x: 700,
+        y: 400,
+        init: true,
+        edges
+      }
+    )
     return () => {
       d3.select(container).selectAll('*').remove()
       // 清除记住的节点坐标
