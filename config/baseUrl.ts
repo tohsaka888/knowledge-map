e8a0004b5314f0c2abbe8257a0e8a801ff48a95c
tohsaka888 <156132264@qq.com>
2022-08-18 16:03:34 +0800

    🧪 test: 修改主路由

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,4 +1,11 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-08-18 16:02:37
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-08-18 16:03:09
+ * @Description: 请填写简介
+ */
 export const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://knowledge-map-git-true-data-tohsaka888.vercel.app'
+    ? 'https://knowledge-map.vercel.app'
     : 'http://172.16.100.149:3000'
