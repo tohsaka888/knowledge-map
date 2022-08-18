c20bd3073d4477d09782b3fb88d25adcd72b2a9e
tohsaka888 <156132264@qq.com>
2022-08-18 17:02:50 +0800

    🎈 perf: 优化延长体验

--- components/KnowledgeMap/utils/calcSize.ts
+++ components/KnowledgeMap/utils/calcSize.ts
@@ -2,7 +2,7 @@
  * @Author: tohsaka888
  * @Date: 2022-08-17 09:28:22
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-17 15:13:14
+ * @LastEditTime: 2022-08-18 17:00:56
  * @Description: 请填写简介
  */
 
@@ -17,8 +17,8 @@ export const calcSize = ({ inData, outData }: Props): number => {
   if (inData.length === 0 && outData.length === 0) {
     return 0
   } else if (inData.length !== 0 && outData.length !== 0) {
-    return 1.1
+    return 1.2
   } else {
-    return 0.1
+    return 0.6
   }
 }
