c1d84c1f28dce102b7b51a57fbd8f1dfb7468ce7
tohsaka888 <156132264@qq.com>
2022-08-18 10:50:45 +0800

    ✨ feat: 节点拖拽事件

--- components/KnowledgeMap/nodeDrag.ts
+++ components/KnowledgeMap/nodeDrag.ts
@@ -2,26 +2,49 @@
  * @Author: tohsaka888
  * @Date: 2022-08-16 17:22:12
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-16 17:25:48
+ * @LastEditTime: 2022-08-18 10:45:24
  * @Description: 请填写简介
  */
 
-// 借助闭包特性
-export const nodeDrag = () => {
-  // 开始拖动
-  const dragStart = () => {
+import * as d3 from 'd3'
+import { Graph } from '../..';
+import { modifyEdge } from './modifyEdge';
+import { verticePrefix } from './prefix';
 
-  }
+type Props = {
+  current: any;
+  event: any;
+  node: Graph.Vertice;
+  config: Graph.ConfigProps;
+  edges: Graph.Line[];
+}
 
-  // 拖动中
-  const dragging = () => {
-    requestAnimationFrame(() => {
+export const dragStart = ({ current }: Props) => {
+  d3.select(current).selectAll('*').style('cursor', 'grabbing');
+}
 
-    })
-  }
+export const dragging = ({ current, event, node, config, edges }: Props) => {
+  node.x = event.x
+  node.y = event.y
+  const { nodeRadius } = config
+  requestAnimationFrame(() => {
+    // 更改节点坐标
+    const container = d3.select(current)
+    container.select(`#${verticePrefix + node.id}`)
+      .attr('cx', event.x)
+      .attr('cy', event.y)
+    container.select(`#${verticePrefix + node.id}text`)
+      .attr('x', event.x)
+      .attr('y', event.y)
+    container.select(`#${verticePrefix + node.id}name`)
+      .attr('x', event.x)
+      .attr('y', event.y + nodeRadius + 10)
+    modifyEdge({ edges, config, x: event.x, y: event.y, node, timer: 0 })
+  })
+}
 
-  // 结束拖动
-  const dragEnd = () => {
-
-  }
+export const dragEnd = ({ current, event, node, config }: Props) => {
+  node.x = event.x
+  node.y = event.y
+  d3.select(current).selectAll('*').style('cursor', 'pointer');
 }
