5ecbf8173debf0aad7ea802172cc666d48426099
tohsaka888 <156132264@qq.com>
2022-08-19 15:22:51 +0800

    ✨ feat: reset时清除memo

--- components/KnowledgeMap/resetCanvas.ts
+++ components/KnowledgeMap/resetCanvas.ts
@@ -2,15 +2,19 @@
  * @Author: tohsaka888
  * @Date: 2022-08-01 11:31:01
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-19 14:20:48
+ * @LastEditTime: 2022-08-19 15:18:43
  * @Description: 请填写简介
  */
 import * as d3 from 'd3'
 import { resetSize, translate } from './canvasDrag'
+import { globalEdges, globalNodes } from './global'
 
 export const resetCanvas = () => {
+  // 清除memo
   translate.x = 0
   translate.y = 0
+  globalNodes.length = 0
+  globalEdges.length = 0
 
   d3.select('#drag')
     .transition()
