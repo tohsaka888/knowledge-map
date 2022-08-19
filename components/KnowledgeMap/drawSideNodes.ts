d19e3d8198bfd0f4485977099e4c76151f186510
tohsaka888 <156132264@qq.com>
2022-08-19 14:25:27 +0800

    🦄 refactor: 重构布局代码

--- components/KnowledgeMap/drawSideNodes.ts
+++ components/KnowledgeMap/drawSideNodes.ts
@@ -20,6 +20,8 @@ import { calcBasicDistence } from "./utils/calcBasicDistance";
 import { verticePrefix } from "./prefix";
 import { createSideNode } from "./createNode";
 import { calcNodePosition } from "./utils/calcNodePosition";
+import { normalLayout } from "./layouts/normal/normalLayout";
+import { paginationLayout } from "./layouts/pagination/paginationLayout";
 
 type Props = {
   typeNodes: Graph.Vertice[][];
@@ -49,67 +51,9 @@ export const drawSideNodes = (
   }: Props
 ) => {
   const container = d3.select('#node-area')
-  const { nodeRadius, arcAreaDistence, arcAreaLength, mode, basicDistence, setVisible } = config
+  const { mode } = config
   typeNodes.forEach((originNodes, index) => {
     const nodes = calcMode(originNodes, 1, mode)
-    const pagination = { page: 1, pageSize: 5 }
-    // if (mode === 2 && originNodes.length > 5) {
-    //   const arc = container
-    //     .append('g')
-    //     .classed('arc', true)
-    //     .append('g')
-    //     .attr('transform', `rotate(${isInside ? -90 - (index + 1) * maxAngle : -90 + index * maxAngle})`)
-    //     .attr('transform-origin', `${centerPoint.x} ${centerPoint.y}`)
-    //   arc
-    //     .append('path')
-    //     .attr('d',
-    //       `M ${centerPoint.x} ${centerPoint.y} 
-    //         m ${arcAreaDistence} 0 
-    //         a ${arcAreaDistence} ${arcAreaDistence} 0 0 1 ${calcArcX(arcAreaDistence, maxAngle)} ${calcArcY(arcAreaDistence, maxAngle)}`
-    //     )
-    //     .attr('fill', 'none')
-    //     .attr('stroke', 'rgba(24, 144, 255, 0.1)')
-    //     .attr('stroke-width', arcAreaLength)
-    //   arc
-    //     .append('g')
-    //     .attr('transform', `translate(${centerPoint.x! + arcAreaDistence - arcAreaLength / 4} ${centerPoint.y})`)
-    //     .append('path')
-    //     .attr('d', 'm0,10.4772l5.5,-9.4772l5.5,9.4772l-2.75,0l0,9.5228l-5.5,0l0,-9.5228l-2.75,0z')
-    //     .attr('fill', '#1890ff')
-    //     .style('cursor', 'pointer')
-    //     .on('mouseover', function () {
-    //       d3.select(this).attr('fill', 'blue')
-    //     })
-    //     .on('mouseleave', function () {
-    //       d3.select(this).attr('fill', '#1890ff')
-    //     })
-    //     .on('click', () => {
-    //       // nextPage(
-    //       //   pagination,
-    //       //   originNodes,
-    //       //   originNodes.length % 5 === 0 ? +(originNodes.length / 5).toFixed(0) : Math.floor(originNodes.length / 5) + 1,
-    //       //   index,
-    //       //   maxAngle,
-    //       //   edges,
-    //       //   config,
-    //       //   isInside,
-    //       //   centerPoint
-    //       // )
-    //     })
-
-    //   arc
-    //     .append('g')
-    //     .classed('arc-text', true)
-    //     .attr('transform', `translate(${centerPoint.x! + arcAreaDistence - arcAreaLength / 5}, ${centerPoint.y})`)
-    //     .append('g')
-    //     .attr('transform-origin', `${-arcAreaDistence + arcAreaLength / 5} ${0}`)
-    //     .attr('transform', `rotate(${maxAngle / 2})`)
-    //     .append('text')
-    //     .attr('transform', `rotate(${90})`)
-    //     .text(`1/${originNodes.length % 5 === 0 ? (originNodes.length / 5).toFixed(0) : Math.floor(originNodes.length / 5) + 1} `)
-    //     .attr('id', `${verticePrefix + nodes[0].labelName}-text`)
-    //     .attr('text-anchor', 'right')
-    // }
 
     const parent = [fId!, ...centerPoint.p!]
     const parentClass = parent.map(p => verticePrefix + p).join(' ')
@@ -118,63 +62,57 @@ export const drawSideNodes = (
       .append('g')
       .classed(nodes[0].labelName, true)
       .classed(parentClass || '', true)
-    let idx = 0
-    nodes.forEach((node) => {
-      node.p = parent
-      if (document.getElementById(verticePrefix + node.id) === null) {
-        // document.getElementById(verticePrefix + node.id)?.parentElement?.remove()
-        // 记录节点信息
-        node.angle = (idx + 1) * maxAngle / (nodes.length + 1) + index * maxAngle
-        node.distance = calcBasicDistence(nodes.length, maxAngle, basicDistence)
-        node.isInside = isInside
-        const position = calcNodePosition(
-          {
-            distance: node.distance,
-            angle: node.angle,
-            centerPoint,
-            isInside: node.isInside,
-            atanAngle,
-            insideLength,
-            outsideLength
-          }
-        )
-        node.x = position.x
-        node.y = position.y
 
-        const sideContainer = typeContainer.append('g')
-        createSideNode({ container: sideContainer, vertice: node, mainVertice: centerPoint, config, edges })
-        window.setTimeout(() => {
-          sideContainer.on('mouseover', function (event) {
-            event.stopPropagation();
-            const x = node.x as number
-            const y = node.y as number
-            d3.select('#border')
-              .attr('transform', `translate(${x}, ${y})`)
-              .select('circle')
-              .attr('stroke-width', 8)
-              .attr('r', nodeRadius + 4)
-            d3.select('#popover-container')
-              .attr('width', nodeRadius * 2 + 10)
-              .attr('height', nodeRadius * 2 + 10)
-              .attr('x', +x)
-              .attr('y', +y - 10)
-            // setVisible && setVisible(true)
-          })
-          // .call(
-          //   d3.drag<any, any, Graph.Node>()
-          //     .on('start', function (event: any, node: Graph.Node) {
-          //       dragStart(this, event, node, config)
-          //     })
-          //     .on('end', function (event: any, node: Graph.Node) {
-          //       dragEnd(this, event, node, config)
-          //     })
-          //     .on('drag', function (event: any, node: Graph.Node) {
-          //       dragging(this, event, node, edges, config)
-          //     })
-          // )
-        }, 1000)
-        idx++
-      }
-    })
+    if (config.mode === 1) {
+      normalLayout(
+        {
+          nodes,
+          maxAngle,
+          index,
+          typeContainer,
+          parent,
+          atanAngle,
+          isInside,
+          insideLength,
+          outsideLength,
+          edges,
+          config,
+          centerPoint
+        }
+      )
+    } else if (config.mode === 2) {
+      paginationLayout({
+        nodes,
+        maxAngle,
+        index,
+        typeContainer,
+        parent,
+        atanAngle,
+        isInside,
+        insideLength,
+        outsideLength,
+        edges,
+        config,
+        centerPoint,
+        originNodes,
+        parentClass,
+      })
+      normalLayout(
+        {
+          nodes,
+          maxAngle,
+          index,
+          typeContainer,
+          parent,
+          atanAngle,
+          isInside,
+          insideLength,
+          outsideLength,
+          edges,
+          config,
+          centerPoint
+        }
+      )
+    }
   })
 }
