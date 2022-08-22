c704c4568b27c6ca18257a515c7ad1dcdc242241
tohsaka888 <156132264@qq.com>
2022-08-22 13:54:40 +0800

    ✨ feat: 添加父节点prefix

--- components/KnowledgeMap/prefix.ts
+++ components/KnowledgeMap/prefix.ts
@@ -2,9 +2,10 @@
  * @Author: tohsaka888
  * @Date: 2022-08-16 15:00:01
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-16 15:00:02
+ * @LastEditTime: 2022-08-22 13:54:03
  * @Description: 请填写简介
  */
 
 export const verticePrefix = 'v-'
 export const edgePrefix = 'e-'
+export const fPrefix = 'f-'
