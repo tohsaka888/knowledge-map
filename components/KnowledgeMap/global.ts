93c9fb3c9852453bf5a9f3d41ad76411461ec634
tohsaka888 <156132264@qq.com>
2022-08-23 16:20:04 +0800

    ✨ feat: clear memo

--- components/KnowledgeMap/global.ts
+++ components/KnowledgeMap/global.ts
@@ -2,24 +2,26 @@
  * @Author: tohsaka888
  * @Date: 2022-08-18 13:32:40
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-22 16:02:56
+ * @LastEditTime: 2022-08-23 16:16:04
  * @Description: 请填写简介
  */
 
 import { Graph } from "../..";
 
-export const globalNodes: Graph.Vertice[] = [];
-export const globalEdges: Graph.Line[] = [];
+export let globalNodes: Graph.Vertice[] = [];
+export let globalEdges: Graph.Line[] = [];
 export let explorePath: {
   mainId: string;
   inData: { vertices: Graph.Vertice[]; edges: Graph.Line[] };
   outData: { vertices: Graph.Vertice[]; edges: Graph.Line[] };
+  isExplore: boolean;
 }[] = []
 
 export let isReset = false
 
 export const filteredPath = (id: string) => {
   explorePath = explorePath.filter(path => path.mainId !== id)
+  globalNodes = globalNodes.filter(n => n.id !== id)
 }
 
 export const changeIsReset = (state: boolean) => {
