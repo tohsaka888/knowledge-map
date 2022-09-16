434ab1ebe6c6fce9323b44d5ae1ffdd2f4e877c7
tohsaka888 <156132264@qq.com>
2022-09-16 08:58:04 +0800

    ✨ feat: use netlify

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -2,10 +2,10 @@
  * @Author: tohsaka888
  * @Date: 2022-08-18 16:02:37
  * @LastEditors: tohsaka888
- * @LastEditTime: 2022-08-18 16:03:09
+ * @LastEditTime: 2022-09-16 08:57:42
  * @Description: 请填写简介
  */
 export const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://knowledge-map.vercel.app'
-    : 'http://172.16.100.149:3000'
+    ? 'https://knowledge-map.netlify.app'
+    : 'http://localhost:3000'
